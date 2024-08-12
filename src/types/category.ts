451832ce749455eec892ed4b06c3b3142cac9fe7
Duy Thanh <tttranduy999@gmail.com>
2024-08-12 12:16:45 +0700

    some page

--- src/types/category.ts
+++ src/types/category.ts
@@ -6,29 +6,33 @@ export interface ICategory {
 }
 
 
-export const sampleCategories: ICategory[] = [
+const sampleCategories: ICategory[] = [
     {
         category_name: 'Áo đầm',
         category_description: 'Latest gadgets and electronics.',
-        category_image: 'https://png.pngtree.com/png-vector/20220304/ourmid/pngtree-women-s-party-dress-no-background-png-image_4476818.png',
+        cagtegory_image: 'https://png.pngtree.com/png-clipart/20231005/original/pngtree-red-dress-for-party-png-image_13123155.png',
         category_total: 150
     },
     {
         category_name: 'Áo',
         category_description: 'A wide range of books from various genres.',
-        category_image: 'https://product.hstatic.net/1000360022/product/id-009311_70121de93add46e4aece989c1aabcd07_master.png',
+        cagtegory_image: 'https://chiinstore.com/media/product/4096_634x634___2022_01_24t140724_196.png',
         category_total: 200
     },
     {
         category_name: 'Quần',
         category_description: 'Trendy and fashionable clothing items.',
-        category_image: 'https://owen.cdn.vccloud.vn/media/catalog/product/cache/323c1569b2962717ca2c8faa79ff3a65/s/v/sv242436.png',
+        cagtegory_image: 'https://owen.cdn.vccloud.vn/media/catalog/product/cache/323c1569b2962717ca2c8faa79ff3a65/q/l/qlbw221082.png',
         category_total: 300
     },
     {
         category_name: 'Đồ bộ',
         category_description: 'Trendy and fashionable clothing items.',
-        category_image: 'https://png.pngtree.com/png-vector/20230808/ourmid/pngtree-christmas-pajama-png-image_6885804.png',
+        cagtegory_image: 'https://195designs.com/cdn/shop/products/IMG_6575_clipped_rev_1.png?v=1639390507',
         category_total: 300
     }
 ];
+
+export {
+    sampleCategories,
+}
