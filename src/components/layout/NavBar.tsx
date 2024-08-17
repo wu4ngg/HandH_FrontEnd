4dc7d2ae872d761022de7a8d0cb69b60a353294d
NguyenDuy <97612815+racingboy2@users.noreply.github.com>
2024-08-17 14:16:08 +0700

    popup login, register

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,124 +1,151 @@
 import { NavLink } from "react-router-dom";
 // import ShoppingCartIcon from 'remixicon-react/ShoppingCartLineIcon'
-import '../../styles/styles.css'
+import "../../styles/styles.css";
 // import ButtonHeaderWidget from "../widget/buttonHeader.widget";
-
 
 import React from "react";
 import HeaderComponentSearch from "../widget/headerComponentSearch";
-import {
-    Menu,
-    Close
-} from "@mui/icons-material";
-import { useSpring, animated } from 'react-spring';
+import { Menu, Close } from "@mui/icons-material";
+import { useSpring, animated } from "react-spring";
 import { ICategory, sampleCategories } from "../../types/category";
 import boxCategory from "../widget/boxCategory.widget";
-
+import PopupComponent from "../popupLogin";
+import PopupRegister from "../popupRegister";
 
 const Navbar: React.FC = () => {
-    const [showCate, setShowCate] = React.useState(false)
-    const springProps = useSpring({
-        opacity: showCate ? 1 : 0,
-        transform: showCate ? 'translateY(0)' : 'translateY(60px)',
-    });
+  const [showCate, setShowCate] = React.useState(false);
+  const [login, setLogin] = React.useState(false);
+  const [register, setRegister] = React.useState(false);
 
-    return (
-        <nav className=" bg-white sticky top-0">
-            <div className="flex mx-auto px-20 py-4 justify-between items-center">
-                <div className="flex justify-start gap-4">
-                    <div className="flex-shrink-0 flex items-center">
-                        <NavLink to="/">
-                            <img className="h-8 w-auto" src="\src\assets\image\logo_header.png" alt="Logo" />
-                        </NavLink>
-                    </div>
-                    <div className="hidden md:block">
-                        <div className="flex space-x-4">
-                            <NavLink
-                                to="/"
-                                className="text-title-nav hover:underline  hover:text-black hover:bg-gray-100 transition-all duration-800 px-3 py-2 rounded-md font-medium"
-                            >
-                                Trang chủ
-                            </NavLink>
-                            <NavLink
-                                to="/shop"
-                                className="text-title-nav hover:underline hover:text-black hover:bg-gray-100 transition-all duration-800 px-3 py-2 rounded-md font-medium"
-                            >
-                                Cửa Hàng
-                            </NavLink>
-                            <NavLink
-                                to="/purchaseOrder"
-                                className="text-title-nav hover:underline hover:text-black hover:bg-gray-100 transition-all duration-800 px-3 py-2 rounded-md font-medium">
-                                Đơn mua
-                            </NavLink>
-                        </div>
-                    </div>
-                </div>
-                <HeaderComponentSearch></HeaderComponentSearch>
+
+  const springProps = useSpring({
+    opacity: showCate ? 1 : 0,
+    transform: showCate ? "translateY(0)" : "translateY(60px)",
+  });
+
+
+  
+  function handleChange() {
+    setLogin(false);
+  }
+  function handleChangeRe() {
+    setRegister(false);
+  }
+
+  return (
+    <nav className=" bg-white sticky top-0">
+      <div className="flex mx-auto px-20 py-4 justify-between items-center">
+        <div className="flex justify-start gap-4">
+          <div className="flex-shrink-0 flex items-center">
+            <NavLink to="/">
+              <img
+                className="h-8 w-auto"
+                src="\src\assets\image\logo_header.png"
+                alt="Logo"
+              />
+            </NavLink>
+          </div>
+          <div className="hidden md:block">
+            <div className="flex space-x-4">
+              <NavLink
+                to="/"
+                className="text-title-nav hover:underline  hover:text-black hover:bg-gray-100 transition-all duration-800 px-3 py-2 rounded-md font-medium"
+              >
+                Trang chủ
+              </NavLink>
+              <NavLink
+                to="/shop"
+                className="text-title-nav hover:underline hover:text-black hover:bg-gray-100 transition-all duration-800 px-3 py-2 rounded-md font-medium"
+              >
+                Cửa Hàng
+              </NavLink>
+              <NavLink
+                to="/purchaseOrder"
+                className="text-title-nav hover:underline hover:text-black hover:bg-gray-100 transition-all duration-800 px-3 py-2 rounded-md font-medium"
+              >
+                Đơn mua
+              </NavLink>
             </div>
-            <div className="flex px-0 py-0  items-center flex-col ">
-                {/* <HeaderBottom></HeaderBottom> */}
-                <div className="flex items-center justify-between w-screen px-20">
-                    {
-                        !showCate ? <div className="pl-4 flex gap-2 items-center bg-button-primary hover:opacity-40 duration-200 ease-linear rounded-xl">
-                            <div className="flex gap-2 items-center">
-                                <Menu className="text-black "></Menu>
-                                <input
-                                    onClick={() => {
-                                        setShowCate(!showCate)
-                                    }}
-                                    className="text-black pr-4 pt-4 pb-4 hover:cursor-pointer"
-                                    type="button"
-                                    value="Danh mục sản phẩm" />
-                            </div>
-                        </div>
-                            :   
-                            <div className="pl-4 flex gap-2 items-center bg-primary-grey hover:opacity-40 duration-200 ease-linear rounded-xl">
-                                <div className="flex gap-2 items-center">
-                                    <Close className="text-black "></Close>
-                                    <input
-                                        onClick={() => {
-                                            setShowCate(!showCate)
-                                        }}
-                                        className="text-black pr-4 pt-4 pb-4 hover:cursor-pointer"
-                                        type="button"
-                                        value="Đóng" />
-                                </div>
-                            </div>
-                    }
-
-                    <div className="flex gap-6">
-                        <div>
-                            <input className="font-bold underline  hover:text-black hover:bg-gray-100 transition-all duration-800 ease-linear hover:cursor-pointer px-4 py-3 rounded-2xl" type="button" value="Đăng nhập" />
-                        </div>
-                        <div>
-                            <input className="font-bold underline  hover:text-black hover:bg-gray-100 transition-all duration-800 ease-linear hover:cursor-pointer px-4 py-3 rounded-2xl" type="button" value="Đăng kí" />
-                        </div>
-                    </div>
-                </div>
-
-                {
-
-                    showCate && (
-                        <div className="bg-black w-screen h-screen bg-opacity-40 mt-2">
-                            <animated.div style={springProps} >
-                                <div className="bg-white absolute left-20 mt-4 w-80 p-4 rounded-3xl">
-                                    {
-                                        sampleCategories.map((category: ICategory, index) => (
-                                            <div key={index}>
-                                                {boxCategory(category)}
-                                            </div>
-                                        ))
-                                    }
-                                </div>
-                            </animated.div >
-                        </div>
-                    )
-                }
-
+          </div>
+        </div>
+        <HeaderComponentSearch></HeaderComponentSearch>
+      </div>
+      <div className="flex px-0 py-0  items-center flex-col ">
+        {/* <HeaderBottom></HeaderBottom> */}
+        <div className="flex items-center justify-between w-screen px-20">
+          {!showCate ? (
+            <div className="pl-4 flex gap-2 items-center bg-button-primary hover:opacity-40 duration-200 ease-linear rounded-xl">
+              <div className="flex gap-2 items-center">
+                <Menu className="text-black "></Menu>
+                <input
+                  onClick={() => {
+                    setShowCate(!showCate);
+                  }}
+                  className="text-black pr-4 pt-4 pb-4 hover:cursor-pointer"
+                  type="button"
+                  value="Danh mục sản phẩm"
+                />
+              </div>
             </div>
-        </nav>
-    )
-}
+          ) : (
+            <div className="pl-4 flex gap-2 items-center bg-primary-grey hover:opacity-40 duration-200 ease-linear rounded-xl">
+              <div className="flex gap-2 items-center">
+                <Close className="text-black "></Close>
+                <input
+                  onClick={() => {
+                    setShowCate(!showCate);
+                  }}
+                  className="text-black pr-4 pt-4 pb-4 hover:cursor-pointer"
+                  type="button"
+                  value="Đóng"
+                />
+              </div>
+            </div>
+          )}
 
+          <div className="flex gap-6">
+            <div>
+              <input
+                onClick={() => setLogin(!login)}
+                className="font-bold underline  hover:text-black hover:bg-gray-100 transition-all duration-800 ease-linear hover:cursor-pointer px-4 py-3 rounded-2xl"
+                type="button"
+                value="Đăng nhập"
+              />
+            </div>
 
-export default Navbar
+            <div>
+              <input
+              onClick={()=>setRegister(!register)}
+                className="font-bold underline  hover:text-black hover:bg-gray-100 transition-all duration-800 ease-linear hover:cursor-pointer px-4 py-3 rounded-2xl"
+                type="button"
+                value="Đăng kí"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* <Popup trigger={false}>
+            <h3 className="bg-black">My popup</h3>
+        </Popup> */}
+        {login && (
+              <PopupComponent handleChange={handleChange}></PopupComponent>
+        )}
+        {register&&(<PopupRegister handleChange={handleChangeRe}></PopupRegister>)}
+
+        {showCate && (
+          <div className="bg-black w-screen h-screen bg-opacity-40 mt-2">
+            <animated.div style={springProps}>
+              <div className="bg-white absolute left-20 mt-4 w-80 p-4 rounded-3xl">
+                {sampleCategories.map((category: ICategory, index) => (
+                  <div key={index}>{boxCategory(category)}</div>
+                ))}
+              </div>
+            </animated.div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/popupRegister.tsx
@@ -0,0 +1,55 @@
+import { Close } from "@mui/icons-material";
+
+function PopupRegister({ handleChange }: any) {
+  return (
+    <div className="overlay w-screen h-screen absolute z-99">
+      <div className="popup-register">
+        <div className="btn-close">
+          <Close onClick={handleChange}></Close>
+        </div>
+        <h1 style={{ fontWeight: "bold" }}>Đăng ký</h1>
+        <input
+          className="bg-primary-grey"
+          type="text"
+          placeholder="Họ tên*"
+          name="UsernameReText"
+          id="UsernameText"
+        />
+        <input
+          className="bg-primary-grey"
+          type="text"
+          placeholder="Email*"
+          name="EmailReText"
+          id="EmailReText"
+        />
+        <input
+          className="bg-primary-grey"
+          type="text"
+          placeholder="Số điện thoại*"
+          name="PhoneReText"
+          id="PhoneReText"
+        />
+        <input
+          className="bg-primary-grey"
+          type="text"
+          placeholder="Mật khẩu*"
+          name="PassReText"
+          id="PassReText"
+        />
+        <input
+          className="bg-primary-grey"
+          type="text"
+          placeholder="Nhập lại mật khẩu"
+          name="RePassReText"
+          id="RePassReText"
+        />
+         <div className="bottom-register">
+            <button className="btn-continute-re">Tiếp tục</button>
+         </div>
+         
+      </div>
+    </div>
+  );
+}
+
+export default PopupRegister;
