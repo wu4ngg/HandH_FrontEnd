5d5a8dd6be573c371ae6ce7727160463f2b538d9
fowardslash <quangrain2014@gmail.com>
2024-09-30 13:38:49 +0700

    sửa filter của shop

--- src/components/widget/sizeWidget.tsx
+++ src/components/widget/sizeWidget.tsx
@@ -1,42 +1,55 @@
 import React from "react";
 import { Size } from "../pages/Shop";
+import { Checkbox } from "../ui/checkbox";
+import { Input } from "../ui/input";
 
 const SizeWidget: React.FC<Size> = (props) => {
+  const { id, isPick, isMore, type, onClick } = props;
 
-    const { id,
-        isPick,
-        isMore,
-        type,
-        onClick
-    } = props
-
-    const [checkMore, setCheckMore] = React.useState(false)
-    return <>
-        <div className="size-box">
-            <div className="size-box_wrap">
-                {
-                    !isMore ? (<div key={id} className="flex gap-2 items-center ">
-                        <input type="checkbox"
-                            onChange={(e) => {
-                                if (onClick) {
-                                    onClick(type, id, e.target.checked)
-                                }
-                            }} className="custom-checkbox" name="" id="" />
-                        <p className="font-bold">{type}</p>
-                    </div>
-                    ) :
-                        (<div key={id} className="flex gap-2 items-center  ">
-                            <input type="checkbox" className="custom-checkbox" onChange={(e) => setCheckMore(e.target.checked)} name="" id="" />
-                            <input type="text" disabled={!checkMore} style={{ opacity: checkMore ? 1 : 0.5 }} className="input-specific bg-slate-300 rounded-3xl
-                                     px-4 py-2 w-[24%] duration-200 ease-in
-                                      outline-none focus:border-none
-                                      align-baseline"  name="" id="" />
-                            <p className="font-bold">{type}</p>
-                        </div>)
-                }
+  const [checkMore, setCheckMore] = React.useState(false);
+  return (
+    <>
+      <div className="size-box">
+        <div className="size-box_wrap">
+          {!isMore ? (
+            <div key={id} className="flex gap-2 items-center ">
+              <Checkbox
+                onCheckedChange={(e) => {
+                  if (onClick) {
+                    onClick(type, id, e as boolean);
+                  }
+                }}
+                className="custom-checkbox"
+                name=""
+                id=""
+              />
+              <p className="font-bold">{type}</p>
             </div>
-        </div >
+          ) : (
+            <div key={id} className="flex gap-2 items-center  ">
+              <Checkbox
+                className="custom-checkbox"
+                onCheckedChange={(e) => setCheckMore(e as boolean)}
+                name=""
+                id=""
+              />
+              <Input
+                type="text"
+                disabled={!checkMore}
+                style={{ opacity: checkMore ? 1 : 0.5 }}
+                className="input-specific  w-[30%] duration-200 ease-in
+                                      outline-none focus:border-none
+                                      align-baseline"
+                name=""
+                id=""
+              />
+              <p className="font-bold">{type}</p>
+            </div>
+          )}
+        </div>
+      </div>
     </>
-}
+  );
+};
 
-export default SizeWidget
+export default SizeWidget;
